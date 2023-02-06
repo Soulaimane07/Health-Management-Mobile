44dd850a57fa16a099001bc8e4c27017afde3c83
Soulaimane <105684836+Soulaimane07@users.noreply.github.com>
2023-02-06 18:12:48 +0100

    images

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Logged/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Soulaimane </Text>
-      <StatusBar style="auto" />
+      <Home />
     </View>
   );
 }
