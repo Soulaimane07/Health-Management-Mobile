ab37a9b7252da12b89091b7d166f89416231d4ee
Soulaimane <105684836+Soulaimane07@users.noreply.github.com>
2023-02-06 18:52:18 +0100

    Home Stack

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import SignStack from './Routes/SignStack';
 import First from './screens/Auth/First';
 import Login from './screens/Auth/Login/Login';
+
+import SignStack from './Routes/SignStack';
+import HomeStack from './Routes/HomeStack';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,7 +18,10 @@ export default function App() {
         
         <Stack.Screen name="start" component={SignStack} />
 
-        
+        <Stack.Screen name="home" component={HomeStack} />
+        <Stack.Screen name="account" component={AccountStack} />
+
+
       </Stack.Navigator>
     </NavigationContainer>
   );
